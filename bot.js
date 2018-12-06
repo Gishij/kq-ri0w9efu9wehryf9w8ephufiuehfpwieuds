3aba57ab8f5c4888f4e86c41d7b182dc85e0768e
Gishij <45468566+Gishij@users.noreply.github.com>
2018-12-06 03:06:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,12 @@ flixz8.on('ready', () => {
     console.log('Ready.');
     var words = ["FuckEdUp","Fucku","ShutUpNiggA","iM38dHhM","NiggaS","NoMerrCy-Nigga","Assaf-BiTcH","iBander","iWantAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
     setInterval(() => {
-        flixz8.channels.find(c => c.id == '493482943537479681').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1500));
+        flixz8.channels.find(c => c.id == '520026772151205893').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1500));
     }, 1500);
 });
 
 flixz8.on('message', message => {
-    if(message.author.id !== '282350776456839169') return;
+    if(message.author.id !== '488388962201763840') return;
     var args = message.content.split(' ');
     var command = message.content.toLowerCase().split(' ')[0];
     if(command == 'apl') {
